7843e36820b0eafeff37501848a7f46dc8728aea
Sebastian Casallas <sebastiancasallasarias2020@gmail.com>
2025-02-26 14:38:19 -0500

    as

--- components/ui/ButtonWhatsapp.tsx
+++ components/ui/ButtonWhatsapp.tsx
@@ -1,6 +1,16 @@
+"use client"
+
 import Link from "next/link";
 
 export default function ButtonWhatsapp() {
+
+  const handleClick = () => {
+    if (typeof window !== 'undefined' && window.fbq) {
+      window.fbq("track", "WhatsAppClick"); 
+    }
+    window.open("https://wa.me/123456789", "_blank");
+  };
+
   return (
     <div className="mt-4">
         <Link href="https://wa.link/1pu11v" target="_blank" className="bg-green-500 p-4 text-xl rounded" passHref>
