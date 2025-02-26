0abd3f73935ff61ca79aafdc32e551b2bd6d7acd
Sebastian Casallas <sebastiancasallasarias2020@gmail.com>
2025-02-26 14:28:22 -0500

    Pixel

--- components/ui/ButtonWhatsapp.tsx
+++ components/ui/ButtonWhatsapp.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ButtonWhatsapp() {
   return (
     <div className="mt-4">
-        <Link href="https://wa.link/bjasvl" target="_blank" className="bg-green-500 p-4 text-xl rounded" passHref>
+        <Link href="https://wa.link/1pu11v" target="_blank" className="bg-green-500 p-4 text-xl rounded" passHref>
             Ver catálogo por WhatsApp
         </Link>
     </div>
