79eb349eb972580f82d7322a058c0dee47f1f704
Sebastian Casallas <sebastiancasallasarias2020@gmail.com>
2025-02-26 15:15:23 -0500

    Pixel tres

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Pixel de Facebook con <Script> */}
         <Script
           id="fb-pixel"
           strategy="afterInteractive"
@@ -33,12 +32,11 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}
               (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${FB_PIXEL_ID}');
+              fbq('init', '996373986005786');
               fbq('track', 'PageView');
             `,
           }}
         />
-        {/* Etiqueta <noscript> para visitantes sin JS */}
         <noscript>
           <Image
             height="1"
